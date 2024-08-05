96910f1e40b29b2384235a1815267ef5c69eed9d
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2024-08-05 04:49:55 +0100

    finish day5 part1

--- src/day5/main.rs
+++ src/day5/main.rs
@@ -1,71 +1,82 @@
-use std::str::FromStr;
+use core::panic;
+use std::u64;
 
 use advent_of_code::open_file::open;
 
 fn main() {
     let content = open("./src/day5/input.txt");
-    let lines = content.lines();
-    let mut map_type: Option<MapType> = None;
+
+    println!("{}", part_one(&content));
+}
+
+fn part_one(input: &String) -> u64 {
+    let mut ans: u64 = u64::MAX;
+    let lines = input.lines();
+    let mut map_type: &str = "";
+    let mut seed_array: Vec<&str> = vec![];
+    let mut maps_array: Vec<Vec<Map>> = Vec::with_capacity(7);
+    for _ in 0..7 {
+        maps_array.push(vec![]);
+    }
     for line in lines {
         let data: Vec<&str> = line.split(':').collect();
         if data.len() == 2 {
-            map_type = match MapType::from_str(data[0]) {
-                Ok(val) => Some(val),
-                Err(_) => None
-            };
+            map_type = data[0].split_whitespace().collect::<Vec<&str>>()[0];
+            if map_type == "seeds" {
+                seed_array = data[1].split_whitespace().collect();
+            }
+            continue;
         }
-        println!("{}", data.len());
-        for i in data {
-            println!("{}", i);
+        load_data(&mut maps_array, data[0], &map_type);
+    }
+    for seed in seed_array {
+        let mut val: u64 = seed.parse().unwrap();
+        for maps in &maps_array {
+            // println!("{}", maps.len());
+            for map in maps {
+                if val >= map.source_start && val <= map.source_end {
+                    val = val - map.source_start + map.destination_start;
+                    break;
+                }
+            }
         }
-    }
-}
-
-fn match_to_map_type(input: &str) -> Option<MapType> {
-    match input {
-        "seed-to-soil" => Some(MapType::SeedToSoil),
-        "soil-to-fertilizer" => Some(MapType::SoilToFertilizer),
-        "fertilizer-to-water" => Some(MapType::FertilizerToWater),
-        "water-to-light" => Some(MapType::WaterToLight),
-        "light-to-temperature" => Some(MapType::LightToTemperature),
-        "temperature-to-humidity" => Some(MapType::TemperatureToHumidity),
-        "humidity-to-location" => Some(MapType::HumidityToLocation),
-        _ => None,
-    }
-}
-
-enum MapType {
-    SeedToSoil,
-    SoilToFertilizer,
-    FertilizerToWater,
-    WaterToLight,
-    LightToTemperature,
-    TemperatureToHumidity,
-    HumidityToLocation,
-}
-
-impl FromStr for MapType {
-    type Err = ();
-
-    fn from_str(input: &str) -> Result<MapType, Self::Err> {
-        match input {
-            "seed-to-soil" => Ok(MapType::SeedToSoil),
-            "soil-to-fertilizer" => Ok(MapType::SoilToFertilizer),
-            "fertilizer-to-water" => Ok(MapType::FertilizerToWater),
-            "water-to-light" => Ok(MapType::WaterToLight),
-            "light-to-temperature" => Ok(MapType::LightToTemperature),
-            "temperature-to-humidity" => Ok(MapType::TemperatureToHumidity),
-            "humidity-to-location" => Ok(MapType::HumidityToLocation),
-            _ => Err(()),
+        if val < ans {
+            ans = val;
         }
     }
+    return ans;
+}
+
+fn load_data(array: &mut Vec<Vec<Map>>, input: &str, map_type: &str) {
+    let numbers: Vec<&str> = input.split_whitespace().collect();
+    if numbers.len() != 3 {
+        return;
+    }
+    let mut map: Map = Map::new();
+    map.load_data(
+        numbers[0].parse().unwrap(),
+        numbers[1].parse().unwrap(),
+        numbers[2].parse().unwrap(),
+    );
+    let index: usize = match map_type {
+        "seed-to-soil" => 0,
+        "soil-to-fertilizer" => 1,
+        "fertilizer-to-water" => 2,
+        "water-to-light" => 3,
+        "light-to-temperature" => 4,
+        "temperature-to-humidity" => 5,
+        "humidity-to-location" => 6,
+        _ => panic!("Map type is empty"),
+    };
+    array[index].push(map);
+    array[index].sort_by(|a, b| a.source_start.cmp(&b.source_start));
 }
 
 struct Map {
-    destination_start: u32,
-    destination_end: u32,
-    source_start: u32,
-    source_end: u32,
+    destination_start: u64,
+    destination_end: u64,
+    source_start: u64,
+    source_end: u64,
 }
 
 impl Map {
@@ -78,7 +89,7 @@ impl Map {
         }
     }
 
-    fn load_data(&mut self, destination_start: u32, source_start: u32, range: u32) {
+    fn load_data(&mut self, destination_start: u64, source_start: u64, range: u64) {
         self.destination_start = destination_start;
         self.source_start = source_start;
         self.destination_end = destination_start + range - 1;
@@ -86,26 +97,41 @@ impl Map {
     }
 }
 
-struct AllMap {
-    seed_to_soil: Vec<Map>,
-    soil_to_fertilizer: Vec<Map>,
-    fertilizer_to_water: Vec<Map>,
-    water_to_light: Vec<Map>,
-    light_to_temperature: Vec<Map>,
-    temperature_to_humidity: Vec<Map>,
-    humidity_to_location: Vec<Map>,
-}
+#[test]
+fn test_part_one() {
+    let input: String = "seeds: 79 14 55 13
 
-impl AllMap {
-    fn new() -> Self {
-        Self {
-            seed_to_soil: vec![],
-            soil_to_fertilizer: vec![],
-            fertilizer_to_water: vec![],
-            water_to_light: vec![],
-            light_to_temperature: vec![],
-            temperature_to_humidity: vec![],
-            humidity_to_location: vec![],
-        }
-    }
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"
+        .to_string();
+    assert_eq!(part_one(&input), 35);
 }
